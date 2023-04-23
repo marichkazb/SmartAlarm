f4422ca65bdbc86eace2f2c1d237680d08fac039
David Rocha <david.miguel.rocha@hotmail.com>
2023-04-23 16:33:06 +0200

    Add Multi Wi-fi capability for Wio

--- wio/wio.ino
+++ wio/wio.ino
@@ -1,11 +1,31 @@
+#include "rpcWiFi.h"
+#include <WiFiMulti.h>
+#include "myEnv.h"
+
+WiFiMulti wifiMulti;
+
 void setup() {
     Serial.begin(115200);
     while(!Serial){ // Wait for Serial to be ready
       delay(500);
     }; 
-    
     Serial.println("Serial is ready.");
-
+    
+    wifiMulti.addAP(SSID_MOBILE, PASSWORD_MOBILE);
+    wifiMulti.addAP(SSID_HOME, PASSWORD_HOME);
+    
+    Serial.println("Connecting Wifi...");
+    if (wifiMulti.run() == WL_CONNECTED) {
+        Serial.println("");
+        Serial.println("WiFi connected");
+        Serial.println("IP address: ");
+        Serial.println(WiFi.localIP());
+    }
 }
 
-void loop() {}
+void loop() {
+    if (wifiMulti.run() != WL_CONNECTED) {
+        Serial.println("WiFi not connected!");
+        delay(1000);
+    }
+}
