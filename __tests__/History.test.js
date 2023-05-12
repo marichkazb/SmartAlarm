d50e5705740683756474e51ee693ccf19190a3ca
milenaxmackowiak <109408342+milenaxmackowiak@users.noreply.github.com>
2023-05-12 08:29:43 +0200

    Made some changes in configuration and created a new test
    Tests for components  still not working, most certainly some more  changes need to be made in configuration files

--- __tests__/History.test.js
+++ __tests__/History.test.js
@@ -1,2 +1,22 @@
-import History from './src/screens/History.js'
+import History from '../src/screens/History';
+
+import '@typescript-eslint/parser';
+import 'parse5'
+import 'jest';
+import '@testing-library/react'
+import '@testing-library'
+import '@testing-library/react'
+import '@testing-library/jest-native'
+import '@testing-library/react-native'
+import { shallow } from 'enzyme'
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import React from 'react';
+
+
+test('renders a container', () => {
+    const wrapper = shallow(React.createElement(History));
+    expect(wrapper.find('View.container')).toHaveLength(1);
+});
+
+
 
