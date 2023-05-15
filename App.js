166e82a6c6305351d8679f78f866c7784885a28d
mariiaz <mariiaz@student.chalmers.se>
2023-05-15 10:11:40 +0200

    Added initialization of the local DB that stores history data

--- App.js
+++ App.js
@@ -3,13 +3,56 @@ import { LogBox } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NativeBaseProvider } from 'native-base';
-import { Home, History, Settings, Sensors} from './src/screens/index';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Home, History, Settings, Sensors } from './src/screens/index';
+import { HISTORY_DB } from './src/constants';
 
 
 const Stack = createNativeStackNavigator();
+
+const date = new Date();
+function formatToLocalString(date) {
+    return date.toLocaleDateString('en-ca');
+}
+
+const intrusionHistory = [
+    {
+        id: Math.random() * 10000,
+        date: formatToLocalString(new Date(Date.UTC(2023, 4, 11))),
+        time: date.toTimeString(),
+        title: 'Security Alarm Triggered',
+        desc: 'Unauthorized attempt to tamper with the security alarm system was detected',
+    },
+    {
+        id: Math.random() * 10000,
+        date: formatToLocalString(new Date(Date.UTC(2023, 4, 12))),
+        time: date.toTimeString(),
+        title: 'Security Alarm Triggered',
+        desc: 'Unauthorized attempt to tamper with the security alarm system was detected',
+    },
+    {
+        id: Math.random() * 10000,
+        date: formatToLocalString(new Date(Date.UTC(2023, 4, 13))),
+        time: date.toTimeString(),
+        title: 'Security Alarm Triggered',
+        desc: 'Unauthorized attempt to tamper with the security alarm system was detected',
+    },
+    {
+        id: Math.random() * 10000,
+        date: formatToLocalString(new Date(Date.UTC(2023, 4, 14))),
+        time: date.toTimeString(),
+        title: 'Security Alarm Triggered',
+        desc: 'Unauthorized attempt to tamper with the security alarm system was detected',
+    }
+];
+const initializeDatabase = async () => {
+    await AsyncStorage.setItem(HISTORY_DB, JSON.stringify(intrusionHistory));
+};
 function App() {
     LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
     LogBox.ignoreAllLogs();//Ignore all log notifications
+    initializeDatabase()
+        . then(r => console.log('Initialized history DB'));
     return (
         <NativeBaseProvider>
             <NavigationContainer>
