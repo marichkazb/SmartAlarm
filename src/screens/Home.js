3028d88c9ca8931fdd4eaef93982119602bc24ea
mariiaz <mariiaz@student.chalmers.se>
2023-05-15 10:16:01 +0200

    Added writing of data to the history database from Home using a test button

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { Image, StyleSheet, Text, View, ScrollView} from 'react-native';
+import { Image, StyleSheet, Text, View, ScrollView } from 'react-native';
 import {
     Button,
     HStack,
-    Switch,
     Avatar,
     Center,
     Progress,
@@ -13,45 +12,72 @@ import {
     Alert,
     Collapse, Box,
 } from 'native-base';
-import { Ionicons, MaterialIcons } from '@expo/vector-icons';
+import { Ionicons, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons';
+import Paho from 'paho-mqtt';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import homeImage from '../assets/HomeBackground.png';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import Paho from "paho-mqtt";
+import { HISTORY_DB } from '../constants';
 
 function Home(props) {
     const [show, setShow] = React.useState(false);
     const { navigation } = props;
 
-    let client;
-    client = new Paho.Client(
-        "broker.hivemq.com",
+    const client = new Paho.Client(
+        'broker.hivemq.com',
         Number(8000),
         `client-id-${parseInt(Math.random() * 100)}`
     );
 
     const topic = 'sensor-status/alarm';
     const [message, setMessage] = useState('not connected');
-
     function onMessage(message) {
-        if (message.destinationName === topic)
-            setMessage(message.payloadString);
+        if (message.destinationName === topic) setMessage(message.payloadString);
     }
 
+    const date = new Date();
+    const getAngleObject = () => ({
+        id: Math.random() * 10000,
+        date: date.toLocaleDateString('en-ca'),
+        time: date.toTimeString(),
+        title: 'Security Alarm Triggered',
+        desc: 'Unauthorized attempt to tamper with the security alarm system was detected',
+    });
+
+    const getMotionObject = () => ({
+        id: Math.random() * 10000,
+        date: date.toLocaleDateString('en-ca'),
+        time: date.toTimeString(),
+        title: 'Motion detected',
+        desc: 'Security alarm detected motion within the secured area',
+    });
+
+    const setHistoryData = async jsonData => {
+        try {
+            const existingDatabase = await AsyncStorage.getItem(HISTORY_DB);
+            const parsedData = existingDatabase ? JSON.parse(existingDatabase) : [];
+            parsedData.push(jsonData);
+            await AsyncStorage.setItem(HISTORY_DB, JSON.stringify(parsedData));
+            console.log('Data added successfully.');
+        } catch (e) {
+            console.log('Error updating storeData:', e);
+        }
+    };
+
     useEffect(() => {
-        client.connect( {
+        client.connect({
             onSuccess: () => {
-                console.log("Connected!");
+                console.log('Connected!');
                 client.subscribe(topic);
                 client.onMessageArrived = onMessage;
             },
             onFailure: () => {
-                console.log("Failed to connect!");
+                console.log('Failed to connect!');
             }
         });
         return () => {
             client.disconnect();
         };
-    }, )
+    },);
 
     function publishTopic(topic, message) {
         const newMessage = new Paho.Message(message);
@@ -61,7 +87,7 @@ function Home(props) {
 
     const turnOn = () => {
         if (!client.isConnected()) {
-            client.connect()
+            client.connect();
         }
         const topic ='wio-command/alarm-activation';
         const message ='on';
@@ -70,7 +96,7 @@ function Home(props) {
 
     const turnOff = () => {
         if (!client.isConnected()) {
-            client.connect()
+            client.connect();
         }
         const topic ='wio-command/alarm-activation';
         const message ='off';
@@ -83,8 +109,8 @@ function Home(props) {
                 <Text style={styles.pageTitle}>Home</Text>
                 <Avatar
                     bg="cyan.500" source={{
-                    uri: 'https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
-                }}>
+                        uri: 'https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+                    }}>
                     TE
                 </Avatar>
             </View>
@@ -99,7 +125,7 @@ function Home(props) {
                     <MaterialIcons name="security" size={50} color="#2420FF" style={{ paddingBottom: 10 }} />
                     <Text>Current status:</Text>
                     <Text>{message}</Text>
-                    <Text></Text>
+                    <Text />
                     <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
                         <Button style={{ backgroundColor: '#D8E59E', padding: 10, borderRadius: 5, marginRight: 10 }} onPress={() => turnOn()}>
                             <Text style={{ color: '#44601A' }}>Turn on</Text>
@@ -128,11 +154,19 @@ function Home(props) {
                     p="5" m="2" borderRadius="md" bg="white" shadow="3"
                     rounded="lg" shaddow="1">
                     <HStack justifyContent="center" flexDirection="column" alignItems="center" width={100} >
-                        <MaterialCommunityIcons name="motion-sensor" size={55} color="#2420FF" style={{ paddingBottom: 30 }}  />
+                        <MaterialCommunityIcons name="motion-sensor" size={55} color="#2420FF" style={{ paddingBottom: 30 }} />
                         <Button onPress={() => navigation.navigate('Sensors')} variant="subtle" colorScheme="blue">Sensors</Button>
                     </HStack>
                 </Center>
             </ScrollView>
+            <Button
+                size="sm" onPress={() => {
+                    setHistoryData(getAngleObject());
+                }
+                } mt={8} mx="auto" style={{ top: -15 }}
+                colorScheme="blue">
+                Add history item
+            </Button>
             <View style={styles.alertContainer}>
                 <Text style={styles.text}>Test alert by clicking this button!</Text>
                 <Button
@@ -154,9 +188,9 @@ function Home(props) {
                                 </HStack>
                                 <IconButton
                                     variant="top-accent" _focus={{
-                                    borderWidth: 0
-                                }} icon={<CloseIcon size="3" />} _icon={{
-                                    color: 'coolGray.600' }} onPress={() => setShow(false)}
+                                        borderWidth: 0
+                                    }} icon={<CloseIcon size="3" />} _icon={{
+                                        color: 'coolGray.600' }} onPress={() => setShow(false)}
                                 />
                             </HStack>
                         </VStack>
