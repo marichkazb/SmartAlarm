3b398755cecbfcfc0ec28b5238b091dfa3b520bd
David Rocha <david.miguel.rocha@hotmail.com>
2023-05-20 00:36:36 +0200

    #113 Adjust Babel config file

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,7 @@
 module.exports = function (api) {
     api.cache(true);
     return {
-        presets: ['babel-preset-expo', '@babel/preset-typescript', "module:metro-react-native-babel-preset", "@babel/preset-react",   '@babel/preset-env' ],
-        plugins: [
-            ["module-resolver", {
-                "alias": {
-                    "^@/(.+)": "./src/\\1"
-                }
-            }],
-            ["@babel/plugin-transform-react-jsx-source"]
-        ]
+        presets: ['babel-preset-expo' ],
+        
     };
 };
