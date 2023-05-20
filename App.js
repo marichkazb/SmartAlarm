03699489c7ae1890ab35435c384f5a6e14f3ea48
David Rocha <david.miguel.rocha@hotmail.com>
2023-05-21 00:48:54 +0200

    #138 Import Platform for the web component

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { LogBox } from 'react-native';
+import { LogBox, Platform } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NativeBaseProvider } from 'native-base';
